8e4606401fb78d21fe3f140a9619a5af24c34234
Maritherecua <mrtrsarroyo@yahoo.com>
2025-11-19 14:34:51 -0800

    js file

---
+++ week04/scripts/objects.js
@@ -0,0 +1,16 @@
+let aCourse = {
+    code: "WDD131",
+    title: "Web Development and Design I",
+    instructor: "Matthew Kearl.",
+    credits: 2,
+};
+function setCourseInfo(course) {
+}
+function sectionTemplate(section) {
+
+}
+function renderSections(sections) {
+
+}
+setCourseInfo(aCourse);
+renderSections(aCourse.sections);
